bd001606d8dd87e0848a2270b0c820809f1fdf82
levenokk <levenokk@yandex.ru>
2020-02-06 00:54:40 +0300

    full adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,22 @@
-$(function(){
+$(function () {
 
+    $('.mobile-btn').on('click', function () {
+        $(this).toggleClass('mobile-btn__active');
+        $('.nav').slideToggle();
+    })
 
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > $(this).height()) {
+            $('.up-arrow').addClass('active');
+        } else {
+            $('.up-arrow').removeClass('active');
+        }
 
-  
+    })
+
+    $('.up-arrow').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    })
 });
